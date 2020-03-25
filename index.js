763cb92966bce680864aeb915ea34d5accbcf388
czy0729 <402731062@qq.com>
2020-03-25 10:20:32 +0800

    no message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getQuery(name) {
   const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`, 'i')
   const r = window.location.search.substr(1).match(reg)
   if (r != null) {
-    return unescape(r[2])
+    return decodeURIComponent(r[2])
   }
   return null
 }
